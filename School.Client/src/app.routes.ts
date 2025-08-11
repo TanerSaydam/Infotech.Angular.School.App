f27f29de70ca767c7b62843b79b9172ba7dce769
Taner Saydam <taner_and_you@hotmail.com>
2025-08-11 21:02:42 +0300

    2.Dersin sonu

---
+++ School.Client/src/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login')
+    },
+    {
+        path: '',
+        loadComponent: () => import('./pages/layouts/layouts'),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/home/home')
+            }
+        ]
+    }
+];
